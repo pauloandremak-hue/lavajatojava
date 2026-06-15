3a0adb1d54f9a5c5dcc6e832216a06cf0cdf2dd1
pauloandremak-hue <pauloandremak@gmail.com>
2026-06-14 22:26:27 -0300

    Add files via upload

---
+++ lavajato/src/lavajato/Cliente.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Pichau
+ */
+package lavajato;
+
+public class Cliente {
+
+    private int codigo;
+    private String nome;
+    private String telefone;
+
+    public Cliente(int codigo, String nome, String telefone) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
